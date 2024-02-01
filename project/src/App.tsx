ae30eb6fc4b6da84d670462d3ae3b969d34a4c18
zxxng <jeayeong123@naver.com>
2024-02-02 00:51:52 +0800

    init: 프로젝트 초기 설정

---
+++ project/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import logo from 'assets/logo.png';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1 className="a11y-hidden">프로그라피 9기 FE 사전 과제</h1>
+        <img src={logo} />
+      </header>
+    </main>
+  );
+}
+
+export default App;
